6005652ce5a370096cbca06437e05ea32f0d3f14
Antony Eufrasio <113488121+Antony-Eufrasio@users.noreply.github.com>
2024-04-16 15:57:44 +0000

    arrumado o bubble

--- exercicio_Bubble.py
+++ exercicio_Bubble.py
@@ -1,6 +1,5 @@
 def bubble(array):
-    for j in range(0, len(array)-1): # Este for é utilizado para que o 
-        programa sempre percorra a lista, excluindo o ultimo valor, já que ele não teria nenhum outro numero para se comparar.
+    for j in range(0, len(array)-1): # Este for é utilizado para que o programa sempre percorra a lista, excluindo o ultimo valor, já que ele não teria nenhum outro numero para se comparar.
         for i in range(0, len(array)-1):
             if array[i]>array[i+1]: # If sendo utilizado para comparar o valor do indice com o proximo.
                 array[i], array[i+1] = array[i+1], array[i] # Caso a condição do if seja atendida, os valores irão trocar de lugar
